ec2f164c07c494f5418f1f4b22ca21d3a8ac37c5
Michael Stallings <stall84@gmail.com>
2021-08-22 09:21:41 -0400

    Added banner datatype and created first entry

---
+++ schemas/banner.js
@@ -0,0 +1,27 @@
+export default {
+  name: "banner",
+  title: "Banner",
+  type: "document",
+  fields: [
+    {
+      title: "Big Text",
+      name: "big_text",
+      type: "string",
+    },
+    {
+      title: "Small Text",
+      name: "small_text",
+      type: "string",
+    },
+    {
+      title: "Button Link",
+      name: "button_link",
+      type: "string",
+    },
+    {
+      title: "Button Text",
+      name: "button_text",
+      type: "string",
+    },
+  ],
+};
